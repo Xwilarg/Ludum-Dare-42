7ab9ddba3739032734e22df459e0def384ad42f9
Xwilarg <xwilarg@yahoo.fr>
2018-08-11 02:01:29 +0200

    Add bridge and defeat condition

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Bridge : MonoBehaviour {
+
+    [SerializeField]
+    private Text lostText;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerController>().didLost = true;
+            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            lostText.gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
     private const float speed = 300f;
 
+    public bool didLost { set; private get; }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -13,6 +15,8 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
+        if (didLost)
+            return;
         rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed;
     }
 }
