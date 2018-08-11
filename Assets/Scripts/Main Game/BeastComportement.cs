6ff106fa3ebd97f2e2198c105e61b6db433da416
Léandre <leandre.blanchard@epitech.eu>
2018-08-11 18:46:45 +0200

    Beast Prefab that follows you

---
+++ Assets/Scripts/Main Game/BeastComportement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeastComportement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject PBar;
+    [SerializeField]
+    private GameObject Beast;
+    [SerializeField]
+    private GameObject Player;
+
+    private float speed = -50;
+    private float maxCapacitor = 18;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+        float rspeed = speed + (280 - Player.GetComponent<PlayerController>().speed);
+        Beast.transform.Translate(new Vector2(rspeed * Time.deltaTime / maxCapacitor, 0f));
+	}
+}
