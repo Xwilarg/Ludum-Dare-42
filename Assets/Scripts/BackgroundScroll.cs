5c11a0382c434d8286a90f35d1e935b34f3876e8
Xwilarg <xwilarg@yahoo.fr>
2018-08-11 02:23:36 +0200

    Add background scroll

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    private const float speed = 4f;
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(0f, -speed * Time.deltaTime));
+        if (transform.position.y < -7.5)
+            transform.Translate(new Vector2(0f, 15f));
+    }
+}
