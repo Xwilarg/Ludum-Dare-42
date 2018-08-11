b1602c64bc3bda9b950a3c63a4d44efc31be7ba5
Xwilarg <xwilarg@yahoo.fr>
2018-08-11 16:43:11 +0200

    Add more dialogs

--- Assets/Scripts/Main Game/Character.cs
+++ Assets/Scripts/Main Game/Character.cs
@@ -1,6 +1,6 @@
 ﻿public struct Character
 {
-    private Character(string name, string description1, string description2, string description3, int weight, string entryLine, string exitLine, string[] lines)
+    private Character(string name, string description1, string description2, string description3, float weight, string entryLine, string exitLine, string[] lines)
     {
         this.name = name;
         this.description1 = description1;
@@ -14,26 +14,28 @@
 
     public readonly string name;
     public readonly string description1, description2, description3;
-    public readonly int weight;
+    public readonly float weight;
     public readonly string entryLine;
     public readonly string exitLine;
     public readonly string[] lines;
 
-    public static Character dummy1 = new Character("Tanya Dezora", "Your little sister.... Wait, what is she doing here ?", "", "", 55,
+    public const float WeightMultiplicator = 0.33f;
+
+    public static Character dummy1 = new Character("Tanya Dezora", "Your little sister.... Wait, what is she doing here ?", "", "", 55f * WeightMultiplicator,
         "Oh, big brother! Nice to see you!", "Eh ? Don't leave me..",
         new string[] {
             "I wonder if mom and dad are okay...",
             "Are we soon arrived ?",
             "I feel safe as long I'm with you, brother"
         });
-    public static Character dummy2 = new Character("Father Rohn", "A priest at the church, will do his best to guide you to do right path.", "", "", 95,
+    public static Character dummy2 = new Character("Father Rohn", "A priest at the church, will do his best to guide you to do right path.", "", "", 95f * WeightMultiplicator,
         "I will guide you my child.", "I forgive you.",
         new string[] {
             "God forgive you for all these people your threw away.",
             "God will guide us through this disaster.",
             "I will pray for you, my child"
         });
-    public static Character dummy3 = new Character("Rikka Leron", "A really energic high school girl... Maybe a bit too much.", "", "", 85,
+    public static Character dummy3 = new Character("Rikka Leron", "A really energic high school girl... Maybe a bit too much.", "", "", 85f * WeightMultiplicator,
         "Yeah, free ride!", "I'm flying!",
         new string[] {
             "Faster! Faster!",
