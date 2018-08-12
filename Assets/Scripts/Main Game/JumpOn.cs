e894fb187070b2eba031cfc8832d533351dcc189
Léandre <leandre.blanchard@epitech.eu>
2018-08-13 00:21:17 +0200

    miniPoutch prefab

---
+++ Assets/Scripts/Main Game/JumpOn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOn : MonoBehaviour {
+
+    private PlayerController Pc;
+    private Rigidbody2D rb;
+    private Transform target;
+   
+    private void Start () {
+        Pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        target = Pc.transform;
+        JumpOnCharacter();
+	}
+
+    private void Update()
+    { 
+        if (transform.position.y <= 0)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+    }
+
+    private void JumpOnCharacter()
+    {
+        float JumpPower = Random.Range(7, 10);
+        rb.gravityScale = 1f;
+        rb.AddForce(new Vector2(JumpPower, JumpPower), ForceMode2D.Impulse);
+    }
+}
