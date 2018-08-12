b3eab84a0c5ba3c6f154aae7e42e4e7574c91c0e
Xwilarg <xwilarg@yahoo.fr>
2018-08-12 17:44:28 +0200

    Add progression for enemy

--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -6,6 +6,8 @@ public class ProgressBar : MonoBehaviour
 {
     private PlayerController player;
     [SerializeField]
+    private GameObject enemy;
+    [SerializeField]
     private RectTransform playerUI, enemyUI;
     private PanelManager pm;
 
@@ -18,14 +20,17 @@ public class ProgressBar : MonoBehaviour
     private void Update()
     {
         if (!player.didLost)
-            playerUI.Translate(new Vector3(player.speed / 6000f, 0f, 0f));
-        if (playerUI.localPosition.x >= 120f)
         {
-            GameObject go = new GameObject("WinInfos", typeof(WinCharacters));
-            DontDestroyOnLoad(go);
-            go.tag = "GameManager";
-            go.GetComponent<WinCharacters>().survivors = pm.PanelList.Select(x => x.b.me).ToList();
-            SceneManager.LoadScene("Win");
+            playerUI.Translate(new Vector3(player.speed / 6000f, 0f, 0f));
+            enemyUI.localPosition = new Vector2(playerUI.localPosition.x - (player.transform.position.x - enemy.transform.position.x), enemyUI.localPosition.y);
+            if (playerUI.localPosition.x >= 120f)
+            {
+                GameObject go = new GameObject("WinInfos", typeof(WinCharacters));
+                DontDestroyOnLoad(go);
+                go.tag = "GameManager";
+                go.GetComponent<WinCharacters>().survivors = pm.PanelList.Select(x => x.b.me).ToList();
+                SceneManager.LoadScene("Win");
+            }
         }
     }
 }
