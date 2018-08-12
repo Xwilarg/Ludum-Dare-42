dffbdbdf82b9c85358f01fd4fddef0bba13cb031
Xwilarg <xwilarg@yahoo.fr>
2018-08-12 18:41:49 +0200

    Add stars

--- Assets/Scripts/Main Game/FenceScroll.cs
+++ Assets/Scripts/Main Game/FenceScroll.cs
@@ -4,21 +4,28 @@ public class FenceScroll : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private PlayerController Pc;
-    private float ScrollSpeed;
-    public float? speed { set; private get; }
+    public float ScrollSpeed { set; get; }
 
     void Start()
     {
         Pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         rb2d = GetComponent<Rigidbody2D>();
-        ScrollSpeed = Pc.speed * Time.deltaTime * 1.5f;
-        rb2d.velocity = new Vector2(-ScrollSpeed, 0);
+        if (rb2d != null)
+        {
+            ScrollSpeed = Pc.speed * Time.deltaTime * 1.5f;
+            rb2d.velocity = new Vector2(-ScrollSpeed, 0f);
+        }
     }
 
     void Update()
     {
-        ScrollSpeed = Pc.speed * Time.deltaTime * 1.5f;
-        rb2d.velocity = new Vector2(-ScrollSpeed, 0);
+        if (rb2d != null)
+        {
+            ScrollSpeed = Pc.speed * Time.deltaTime * 1.5f;
+            rb2d.velocity = new Vector2(-ScrollSpeed, 0f);
+        }
+        else
+            transform.Translate(new Vector2(-ScrollSpeed * Time.deltaTime, 0f));
         if (transform.position.x < -17 && (tag == "Character" || tag == "Trap"))
             Destroy(gameObject);
     }
--- Assets/Scripts/Managers/StarManager.cs
+++ Assets/Scripts/Managers/StarManager.cs
@@ -7,7 +7,7 @@ public class StarManager : MonoBehaviour
     [SerializeField]
     private Sprite[] stars;
 
-    private readonly Vector2 refTimer = new Vector2(0.001f, 0.1f);
+    private readonly Vector2 refTimer = new Vector2(0.001f, 0.01f);
     private float timer;
 
     private void Start()
@@ -24,7 +24,7 @@ public class StarManager : MonoBehaviour
             GameObject go = Instantiate(starPrefab, new Vector2(15f, Random.Range(-5f, 5f)), Quaternion.identity);
             go.GetComponent<SpriteRenderer>().sprite = stars[Random.Range(0, stars.Length)];
             go.transform.localScale = new Vector2(.1f, .1f);
-            go.GetComponent<FenceScroll>().speed = Random.Range(0.1f, 1f);
+            go.GetComponent<FenceScroll>().ScrollSpeed = Random.Range(10f, 100f);
         }
     }
 }
