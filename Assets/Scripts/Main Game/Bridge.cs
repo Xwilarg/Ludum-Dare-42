f0e9d30d51b2671941f199ccf04e34593c00ffab
Léandre <leandre.blanchard@epitech.eu>
2018-08-12 13:44:33 +0200

    scrolling fences

--- Assets/Scripts/Main Game/Bridge.cs
+++ Assets/Scripts/Main Game/Bridge.cs
@@ -3,9 +3,11 @@
 [RequireComponent(typeof(BoxCollider2D))]
 public class Bridge : MonoBehaviour
 {
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.GetComponent<PlayerController>().isJumping)
             collision.GetComponent<PlayerController>().Loose();
     }
+
 }
---
+++ Assets/Scripts/Main Game/FenceScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceScroll : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private PlayerController Pc;
+    private float ScrollSpeed;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        ScrollSpeed = Pc.speed / 50;
+        rb2d.velocity = new Vector2(-ScrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ScrollSpeed = Pc.speed / 50;
+        rb2d.velocity = new Vector2(-ScrollSpeed, 0);
+    }
+}
