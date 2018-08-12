ad3e9085d56d2871f6a6d20d8a98d981701b1114
Xwilarg <xwilarg@yahoo.fr>
2018-08-12 03:12:45 +0200

    Add beast

--- Assets/Scripts/Main Game/BeastComportement.cs
+++ Assets/Scripts/Main Game/BeastComportement.cs
@@ -7,21 +7,14 @@ public class BeastComportement : MonoBehaviour
     [SerializeField]
     private GameObject PBar;
     [SerializeField]
-    private GameObject Beast;
-    [SerializeField]
     private GameObject Player;
 
-    private float speed = -50;
+    private float speed = -20;
     private float maxCapacitor = 18;
-
-	void Start ()
-    {
-	    
-	}
 	
 	void Update ()
     {
         float rspeed = speed + (280 - Player.GetComponent<PlayerController>().speed);
-        Beast.transform.Translate(new Vector2(rspeed * Time.deltaTime / maxCapacitor, 0f));
+        transform.Translate(new Vector2(rspeed * Time.deltaTime / maxCapacitor, 0f));
 	}
 }
