b4c099826daa9704fed444b47dfaca3c76a02b3a
Xwilarg <xwilarg@yahoo.fr>
2018-08-12 23:49:18 +0200

    Try to rotate projectile

---
+++ Assets/Scripts/Main Game/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        transform.rotation = 
+            Quaternion.Euler(0f, 0f, Quaternion.LookRotation(transform.position + new Vector3(rb.velocity.x, rb.velocity.y, 0f)).eulerAngles.z);
+    }
+}
