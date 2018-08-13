fd7d7ac5ebc109e67b20afe4e775cac3bc128c1e
Léandre <leandre.blanchard@epitech.eu>
2018-08-13 20:22:20 +0200

    Text a la fin

---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    // Use this for initialization
+    private Almanac Al;
+    private Text text;
+
+	void Start () {
+        Al = GameObject.FindGameObjectWithTag("Almanac").GetComponent<Almanac>();
+        gameObject.GetComponent<Text>().text = "Your final score: <b>" + Al.score.ToString() + "</b>" + System.Environment.NewLine + System.Environment.NewLine + gameObject.GetComponent<Text>().text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
