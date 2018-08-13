292e2c9b321cfbfc7a2a9b7eb80c3e69e41e81f7
Xwilarg <xwilarg@yahoo.fr>
2018-08-13 12:30:29 +0200

    Fix fearful

--- Assets/Scripts/Main Game/CharacterPanelDelete.cs
+++ Assets/Scripts/Main Game/CharacterPanelDelete.cs
@@ -67,14 +67,14 @@ public class CharacterPanelDelete : MonoBehaviour
             else if (me.me.classe == Character.Classe.Fearful)
             {
                 speTimer = 5f;
-                pc.speed *= 1.5f;
+                pc.speed += 150f;
             }
         }
         if (speTimer < 0f && speTimer > -1f)
         {
             if (me.me.classe == Character.Classe.Fearful)
             {
-                pc.speed /= 1.5f;
+                pc.speed -= 150f;
                 ResetCooldown();
             }
             speTimer = -1.5f;
