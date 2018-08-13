097521e3c62e1016e2259109a47c013fafcc7ac8
Léandre <leandre.blanchard@epitech.eu>
2018-08-13 12:36:13 +0200

    oui

--- Assets/Scripts/Main Game/CharacterPanelDelete.cs
+++ Assets/Scripts/Main Game/CharacterPanelDelete.cs
@@ -67,14 +67,14 @@ public class CharacterPanelDelete : MonoBehaviour
             else if (me.me.classe == Character.Classe.Fearful)
             {
                 speTimer = 5f;
-                pc.speed += 150f;
+                pc.speed += 5f;
             }
         }
         if (speTimer < 0f && speTimer > -1f)
         {
             if (me.me.classe == Character.Classe.Fearful)
             {
-                pc.speed -= 150f;
+                pc.speed -= 5f;
                 ResetCooldown();
             }
             speTimer = -1.5f;
