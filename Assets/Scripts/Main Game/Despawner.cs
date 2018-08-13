71a872cdfa207fed9c6c5895a09edb8f79467568
Xwilarg <xwilarg@yahoo.fr>
2018-08-13 17:31:33 +0200

    Now despawn love

---
+++ Assets/Scripts/Main Game/Despawner.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Despawner : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.position.x < -30f)
+            Destroy(gameObject);
+    }
+}
